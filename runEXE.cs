34e99e06641cb875e263e7e49ccd449795cb5f62
MoonJing <jing1999fan@outlook.com>
2022-04-19 03:00:18 -0500

    Add files via upload
    1st version

---
+++ runEXE.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace runEXE
+{
+    class runEXE
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Process myProcess = new Process ())
+                {
+                    myProcess.StartInfo.UseShellExecute = true;
+                    myProcess.StartInfo.FileName = @"c:\Users\admin\source\repos\DirectoryStatistic\DirectoryStatistic\bin\Debug\DirectoryStatistic.exe";
+                    myProcess.Start();
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
